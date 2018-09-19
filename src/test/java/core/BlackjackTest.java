be237dfcfc0ca92737304e64d6bd7960e039910e
fadifadi22 <fadifadi22@live.ca>
2018-09-19 01:44:57 -0400

    Test: check to see if players hand increases

--- src/test/java/core/BlackjackTest.java
+++ src/test/java/core/BlackjackTest.java
@@ -1,10 +1,31 @@
 package core;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import core.Blackjack;
 
 
-public class BlackjackTest extends TestCase {
+public class BlackjackTest {
 	
+
+	@Test
+	public void playerCardsTest()
+	{
+		Blackjack playerCards = new Blackjack();
+		playerCards.toString(SEVEN);
+		assertEquals(1 , playerCards.toString());
+	}
+	
+	@Test
+	public void toStringTest()
+	{
+		Blackjack playerCards = new Blackjack();
+		playerCards.addCard(SEVEN);
+		playerCards.addCard(A);
+		playerCards.addCard(J);
+
+		assertEquals("SEVEN A J", playerCards.toString());
+	}
 	
 
 }
