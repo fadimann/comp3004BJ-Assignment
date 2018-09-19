c771945b8c9675455a50ca20dc337b6126563f6d
fadifadi22 <fadifadi22@live.ca>
2018-09-19 00:07:20 -0400

    test: checked for card values

--- src/test/java/core/DeckTest.java
+++ src/test/java/core/DeckTest.java
@@ -2,6 +2,7 @@ package core;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import core.Deck;
 
 
 public class DeckTest {
@@ -12,5 +13,20 @@ public class DeckTest {
 		Deck deck = new Deck();
 		assertEquals(52, deck.createFullDeck());
 	}
+	
+	@Test
+	public void cardsValueTest()
+	{
+		Deck J = new Deck();
+		assertEquals(10, J.cardsValue());
+		
+		Deck Q = new Deck();
+		assertEquals(10, Q.cardsValue());
+		
+		Deck K = new Deck();
+		assertEquals(10, K.cardsValue());
+	}
+
+	
 	
 }
