9dabc606a2ff28886bc0bb3859fdeda5b173316f
fadifadi22 <fadifadi22@live.ca>
2018-09-19 13:05:32 -0400

    More tests on getting cards out of the deck + adding new hand totals.

--- src/test/java/core/DeckTest.java
+++ src/test/java/core/DeckTest.java
@@ -26,6 +26,45 @@ public class DeckTest {
 		Deck K = new Deck();
 		assertEquals(10, K.cardsValue());
 	}
+	
+	@Test
+	public void getCard()
+	{
+		Deck playerHand = new Deck();
+		int deckSize = 52;
+
+		assertNotSame(deckSize, deckSize - 1);
+	}
+	@Test
+	public void addCard()
+	{
+		Deck playerHand = new Deck();
+		playerHand.addCard(J);
+		playerHand.addCard(J);
+		playerHand.addCard(J);
+
+		assertEquals(30, playerHand.getTotalHandValue());
+	}
+	
+	@Test
+	public void getCard()
+	{
+		Deck dealerHand = new Deck();
+		int deckSize = 52;
+
+		assertNotSame(deckSize, deckSize - 1);
+	}
+	@Test
+	public void addCard()
+	{
+		Deck dealerHand = new Deck();
+		dealerHand.addCard(J);
+		dealerHand.addCard(J);
+		dealerHand.addCard(J);
+
+		assertEquals(30, dealerHand.getTotalHandValue());
+	}
+
 
 	
 }
