0d7c3d5e70b88eea8f502eafb6805c0387fc31d9
fadifadi22 <fadifadi22@live.ca>
2018-09-20 00:06:39 -0400

    Deck; print out method

--- src/main/java/core/Deck.java
+++ src/main/java/core/Deck.java
@@ -49,12 +49,28 @@ public class Deck {
     	}
     }
 
+    public Card dealNextCard(){
+    	Card top = this.myCards[0];
+    	
+    	for (int c = 1; c < this.numCards; c++){
+    		this.myCards[c-1] = this.myCards[c];
+    	}
+    	this.myCards[this.numCards-1] = null;
+    	
+    	this.numCards--;
+    	
+    	return top;
+    }
     
+    public void printDeck(int numToPrint){
+    	for(int c = 0; c < numToPrint; c++){
+    		System.out.printf("% 3d/%d %s\n", c+1, this.numCards, this.myCards[c].toString());
+    	}
+    }
+
+
+
 }
  
 
     	
-    
-
-
-		
